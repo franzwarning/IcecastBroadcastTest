a581fa263300906d375ce30670c1a8e3e69a08ab
Raymond Kennedy <george@getjotdown.com>
2019-04-07 19:52:14 +0200

    First commit

---
+++ IcecastTest/Icecast-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  Icecast-Bridging-Header.h
+//  IcecastTest
+//
+//  Created by Raymond Kennedy on 4/7/19.
+//  Copyright © 2019 Sunflower Software Inc. All rights reserved.
+//
+
+#ifndef Icecast_Bridging_Header_h
+#define Icecast_Bridging_Header_h
+
+#import "liblame.h"
+#import "lame.h"
+#import "libshout.h"
+
+#endif /* Icecast_Bridging_Header_h */
